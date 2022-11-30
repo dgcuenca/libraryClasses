705aa4dd3fa0553a3d5ca5fdf1f9c0798c8ba063
Glenda Diaz <glendadiazah@outlook.com>
2022-11-30 15:37:59 -0600

    add js comand to change from section  to section and add contact section

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,7 +1,28 @@
 const listBook = document.getElementById('bookList');
 const bookAdd = document.getElementById('addBook');
 const contact = document.getElementById('contact');
+const mainScreen = document.querySelector('#main-screen');
+const bookListScreen = document.querySelector('.noShow');
+const contactSection = document.querySelector('#contact-section');
 
-listBook.addEventListener('click', () => {
-    document.querySelector('#booklistSection').classList.toggle('show');
-  });
+function displayBookList(){
+  bookListScreen.style.display = 'block';
+  mainScreen.style.display = 'none';
+  contactSection.style.display = 'none';
+}
+
+function displayAddBook(){
+  mainScreen.style.display = 'block';
+  bookListScreen.style.display = 'none';
+  contactSection.style.display = 'none';
+}
+
+function displayContact(){
+  mainScreen.style.display = 'none';
+  bookListScreen.style.display = 'none';
+  contactSection.style.display = 'block';
+}
+
+listBook.addEventListener('click', displayBookList);
+bookAdd.addEventListener('click', displayAddBook);
+contact.addEventListener('click', displayContact);
